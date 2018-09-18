5061c7ba3c09d289affda250053cfc76c5855142
andres-root <info@andresroot.co>
2018-09-17 22:40:15 -0500

    WIP

--- linaltools.py
+++ linaltools.py
@@ -30,3 +30,8 @@ def sum(self, vectors):
 def sub(self, vectors):
     # TODO: Add valitadion for vector size
     return [reduce(lambda a, b: a - b, x) for x in zip(*vectors)]
+
+
+# Multiply a vector by a scalar
+def mult(self, scalar, vector):
+    return [(scalar * x) for x in vector]
