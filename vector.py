8547adf13bef9cfaa32cfcdf058a1b689282f3b1
andres-root <info@andresroot.co>
2018-09-17 21:46:55 -0500

    WIP

--- vector.py
+++ vector.py
@@ -1,20 +1,21 @@
-
-
-class Vector:
+class Vector(object):
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
             self.coordinates = tuple(coordinates)
             self.dimension = len(coordinates)
+
         except ValueError:
             raise ValueError('The coordinates must be nonempty')
 
         except TypeError:
-            raise TypeError('The coordinates ust be an iterable')
+            raise TypeError('The coordinates must be an iterable')
+
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
+
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
